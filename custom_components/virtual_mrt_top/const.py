8012069d34d7560f1bbb82082f0de56e0d6be419
baudneo <86508179+baudneo@users.noreply.github.com>
2025-12-02 19:12:20 -0700

    auto modeling using surface temperature sensor to output a calibrated k_loss (insulation loss) sensor.

--- custom_components/virtual_mrt_top/const.py
+++ custom_components/virtual_mrt_top/const.py
@@ -27,6 +27,7 @@ CONF_HVAC_AIR_SPEED = "hvac_air_speed"
 CONF_RADIANT_SURFACE_TEMP = "radiant_surface_temp"
 CONF_RADIANT_TYPE = "radiant_type"
 CONF_RH_SENSOR = "rh_sensor"
+CONF_WALL_SURFACE_SENSOR = "wall_surface_sensor"
 CUSTOM_PROFILE_KEY = "custom"
 
 ORIENTATION_OPTIONS = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
